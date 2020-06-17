1f393d8725c1e87782b41e1152d64d841d0a9ee3
Jose Maria Chadid <jose.chadid@restituciondetierras.gov.co>
2020-06-17 08:53:33 -0500

    Resolve inverse matrix
    basecase
    makeCacheMatrix(matrix(nrow = 2,ncol = 2,c(5,-7,2,-3)))

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,17 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+m<-NULL
+solve_m<-NULL
 
+## Write a short comment describing this function
+## store m (parametre of the function) and its inverse (solve_m) 
 makeCacheMatrix <- function(x = matrix()) {
+  if(!identical(x,m)) {return (cacheSolve(x))}
+  else {
+    print("already exist")
+    return(cacheSolve(m))
+  }
 
 }
 
@@ -12,4 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(solve_m)
+  }
+  print("not exist before")
+  m<<-x
+  solve_m <<- solve(m)
+  return(solve_m)
 }
